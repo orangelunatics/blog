322b16dd814a374f101730ea8bc4cd3103851248
“T1otto” <xinkuie@hotmail.com>
2021-04-17 15:08:12 +0800

    second

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,12 +1,18 @@
 module.exports = {
   base: "/blog/",
+  title: "Chase the Truth 🙏🏻",
+  description: "navi的个人blog",
+  head: [
+    ['meta', { name: 'author', content: 'navi' }],
+    ['meta', { name: 'keywords', content: '阿里巴巴字节跳动腾讯' }],
+    ['link', { rel: 'icon', href: '/favicon.ico' }]
+  ],
   themeConfig: {
+    lastUpdated: 'Last Updated',
     logo: '/assets/img/hero.png',
     nav: [
       { text: 'Home', link: '/' },
-      { text: 'Home', link: '/' },
-      { text: 'Home', link: '/' },
-      { text: 'Home', link: '/' },
+      { text: '关于前端', link: '/introduction/' },
       { text: 'Guide', link: '/about/' },
       {
         text: 'Languages',
---
+++ docs/introduction.md
@@ -0,0 +1,25 @@
+---
+# sidebar: auto
+---
+
+##  Navi
+## 1-学习路线
+HTML5🌻  CSS3🌻  ES6🌻  DOM🌻  BOM🌻  Git部分🌻  Node部分🌻  Webpack🌻  Vue🌻
+
+## 2-近期计划
+Node深入学习🌼  React🌼  TypeScript🌼  Git熟悉使用🌼
+
+## 3-前端方向&趋势
+### 3-1 跨端
+RN Weex Flutter uni-app
+### 3-2 工程化
+基于node的一系列构建工具
+### 3-3 可视化
+动画 VR 3D模型
+three.js  Echarts  canvas  WebGL
+### 3-4 智能化
+Tensorflow.js
+### 3-5 5G
+
+### 3-6 编辑器
+VSCode  HBuilder  云端IDE
