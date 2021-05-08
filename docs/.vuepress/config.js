cb950d3da0d0d7aa2a424b3c07bcd0fef7bdd991
“T1otto” <xinkuie@hotmail.com>
2021-05-08 23:04:52 +0800

    投机

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -6,6 +6,7 @@ module.exports = {
     ['meta', { name: 'author', content: 'navi' }],
     ['meta', { name: 'keywords', content: 'navi' }],
     ['link', { rel: 'icon', href: '/favicon.ico' }],
+    ['meta', { httpEquiv: 'Content-Security-Policy', content: 'upgrade-insecure-requests' }],
     ['script',{type: 'text/javascript', src: 'http://rf.revolvermaps.com/0/0/6.js?i=58kyuv0rzom&amp;m=7&amp;c=e63100&amp;cr1=ffffff&amp;f=arial&amp;l=0&amp;bv=90&amp;lx=-420&amp;ly=420&amp;hi=20&amp;he=7&amp;hc=a8ddff&amp;rs=80',async: 'async'}]
   ],
   plugins: ['go-top'],
