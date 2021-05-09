a37dcd21b259f20755d17d0b2940d6b271dfa9e0
“T1otto” <xinkuie@hotmail.com>
2021-05-09 10:25:56 +0800

    1

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -6,7 +6,7 @@ module.exports = {
     ['meta', { name: 'author', content: 'navi' }],
     ['meta', { name: 'keywords', content: 'navi' }],
     ['link', { rel: 'icon', href: '/favicon.ico' }],
-    ['script',{type: 'text/javascript', src: '//rf.revolvermaps.com/0/0/6.js?i=58kyuv0rzom&amp;m=7&amp;c=e63100&amp;cr1=ffffff&amp;f=arial&amp;l=0&amp;bv=90&amp;lx=-420&amp;ly=420&amp;hi=20&amp;he=7&amp;hc=a8ddff&amp;rs=80',async: 'async'}]
+    ['script',{type: 'text/javascript', src: 'https://rf.revolvermaps.com/0/0/6.js?i=58kyuv0rzom&amp;m=7&amp;c=e63100&amp;cr1=ffffff&amp;f=arial&amp;l=0&amp;bv=90&amp;lx=-420&amp;ly=420&amp;hi=20&amp;he=7&amp;hc=a8ddff&amp;rs=80',async: 'async'}]
   ],
   plugins: ['go-top'],
   themeConfig: {
