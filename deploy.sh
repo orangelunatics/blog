c9fc3e58df73987bf5e5b15c73df2ebd9bbefb3f
orangelunatics <xinkuie@hotmail.com>
2022-11-14 20:16:52 +0800

    feat: 更改部署域名

--- deploy.sh
+++ deploy.sh
@@ -20,6 +20,6 @@ git commit -m 'deploy'
 # git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git master
 
 # 如果发布到 https://<USERNAME>.github.io/<REPO>
-git push -f git@github.com:T1otto/blog.git master:gh-pages
+git push -f git@github.com:orangelunatics/blog.git master:gh-pages
 
 cd -
